2317c5c59c88cf1ddc94b7630aacad152cd48ce6
Andres Felipe De La Ossa Navarro <andresdelaossa@hotmail.com>
2019-08-10 09:10:25 -0500

    Initial Commit

---
+++ LAHackatonIoT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LAHackatonIoT
+//
+//  Created by Andres Felipe De La Ossa Navarro on 8/10/19.
+//  Copyright © 2019 Andres Felipe De La Ossa Navarro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
